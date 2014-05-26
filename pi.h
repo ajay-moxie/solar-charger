14dc4089ef4a09613cd780434ef2c9db7cfe6b12
Ajay <ajay@moxiedevices.com>
2014-05-26 23:00:57 +0530

    pi: top level include file
    Added top level include file for all pi related definitions.
    
    Signed-off-by: Ajay <ajay@moxiedevices.com>

---
+++ pi.h
@@ -0,0 +1,21 @@
+#ifndef __PI_H__
+#define __PI_H__
+typedef enum{
+	PI_ON,
+	PI_OFF,
+}state_t;
+
+
+struct pi{
+	uint8_t Kp;
+	uint8_t Ki;
+	uint16_t duty_max;
+	uint16_t setpoint;
+	uint16_t feedback;
+	int16_t error;
+	int16_t integral;
+	uint16_t duty_cycle;
+	uint8_t saturated;
+	state_t state;
+};
+#endif
