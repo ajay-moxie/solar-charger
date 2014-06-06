cb0cba7d97de9bafe8d9408277ddd17b20aa87c3
Ajay <ajay@moxiedevices.com>
2014-06-06 11:39:29 +0530

    pwm.h: removed commented portion
    Signed-off-by: Ajay <ajay@moxiedevices.com>

--- PWM.h
+++ PWM.h
@@ -12,16 +12,6 @@
 #define DUTY_CYCLE_10_L ((DUTY_CYCLE_10 & 0x03) << 6)
 
 
-/*uint8_t Kp;
-uint8_t Ki;
-uint16_t duty_max;
-uint16_t setpoint;
-uint16_t feedback;
-int16_t error;
-int16_t integral;
-uint16_t duty_cycle;
-uint8_t saturated;
-*/
 //PWM functions
 void disable_pwm1(void);
 void enable_pwm1(void);
