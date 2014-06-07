f1a221b9c8010b19c16d42aece6cbe0716f89b85
Ajay <ajay@moxiedevices.com>
2014-06-07 13:59:23 +0530

    led load: power management driver support
    Signed-off-by: Ajay <ajay@moxiedevices.com>

--- Load_Switch.c
+++ Load_Switch.c
@@ -1,6 +1,10 @@
-
+#include "power_mgmt.h"
 #include "Load_Switch.h"
+#include "common.h"
+#include "config.h"
 
+static void load_switch_sleep_exit(void);
+static void load_switch_sleep_prepare(void);
 /*void config_load_switch (void) {
   ANSC3 = 0;
   TRISC3 = 0;
@@ -15,12 +19,20 @@ void disable_load_switch(void) {
 }
 
 void load_switch_init(void) {
+  load_switch_sleep_exit();
+#ifdef LED_LOAD_POWER_MGMT
+  register_prepare_sleep(load_switch_sleep_prepare);
+  register_post_sleep(load_switch_sleep_exit);
+#endif
+} 
+
+static void load_switch_sleep_exit(void) {
   disable_load_switch();
   ANSC3 = 0;
   TRISC3 = 0;
-} 
+}
 
-void load_switch_exit(void)
+static void load_switch_sleep_prepare(void)
 {
   disable_load_switch();
   ANSC3 = 1;
