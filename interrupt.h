cc1e1654140026e1acefcf8ad6e07d337b5a958a
Ajay <ajay@moxiedevices.com>
2014-09-06 00:22:01 +0530

    defined interrupt type
    Signed-off-by: Ajay <ajay@moxiedevices.com>

--- interrupt.h
+++ interrupt.h
@@ -1,11 +1,12 @@
-
+#ifndef __INTERRUPT_H__
+#define __INTERRUPT_H__
 #include <htc.h>
 #include <stdint.h>
 
-
-uint8_t start_pi = 0;
-
-
+typedef enum{
+	RISING_EDGE,
+	FALLING_EDGE,
+}interrupt_type_t;
 //function declaration
 void enable_IOC_interrupt(void);
 void disable_IOC_interrupt(void);
@@ -14,3 +15,4 @@ void interrupt update_intensity_flags_int(void);
 void configure_interrupt(void);
 void DI();
 void EI();
+#endif
